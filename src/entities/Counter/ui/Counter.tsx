3766477b5642c260c5409ae296740d7fb7c1c34a
Polishchuk <s.polishchuk@amiga.agency>
2023-04-10 00:29:29 +0700

    add redux store, first test entity

---
+++ src/entities/Counter/ui/Counter.tsx
@@ -0,0 +1,43 @@
+import { FC } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
+import { counterActions } from '../model/slice/counterSlice';
+import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+
+interface CounterProps {
+className?: string;
+}
+
+export const Counter: FC<CounterProps> = ({ className }) => {
+    const dispatch = useDispatch();
+    const counterValue = useSelector(getCounterValue);
+    const { t } = useTranslation();
+
+    const incement = () => {
+        dispatch(counterActions.increment());
+    };
+
+    const decrement = () => {
+        dispatch(counterActions.decrement());
+    };
+    return (
+        <div>
+            <h1 data-testid="value-title">
+                {counterValue}
+            </h1>
+            <Button
+                onClick={incement}
+                data-testid="increment-btn"
+            >
+                {t('increment')}
+            </Button>
+            <Button
+                onClick={decrement}
+                data-testid="decrement-btn"
+            >
+                {t('decrement')}
+            </Button>
+        </div>
+    );
+};
